745ba233735eb9872c9e59d1fa718728ec575ea5
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2024-04-16 11:51:23 +0900

    Updated: japanese ime support for different types

--- windows/windows_ime_manager_plugin.cpp
+++ windows/windows_ime_manager_plugin.cpp
@@ -3,9 +3,6 @@
 // This must be included before many other Windows headers.
 #include <windows.h>
 
-// For getPlatformVersion; remove unless needed for your plugin implementation.
-#include <VersionHelpers.h>
-
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
@@ -47,32 +44,54 @@ namespace windows_ime_manager
       const flutter::MethodCall<flutter::EncodableValue> &method_call,
       std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
   {
-    if (method_call.method_name() == "japaneseImeOn" || method_call.method_name() == "japaneseImeOff")
+    if (method_call.method_name() == "japaneseHiraganaIme" ||
+        method_call.method_name() == "japaneseFullWidthKatakana" ||
+        method_call.method_name() == "japaneseHalfWidthKatakana" ||
+        method_call.method_name() == "japaneseHalfWidthAlphanumeric")
     {
       HKL japaneseIME = LoadKeyboardLayout(TEXT("00000411"), KLF_ACTIVATE);
 
       if (japaneseIME == NULL)
       {
         std::cout << "Failed to load Japanese IME" << std::endl;
+        result->Error("IME Load Failure", "Failed to load Japanese IME");
+        return;
       }
-      else
+
+      if (!ActivateKeyboardLayout(japaneseIME, 0))
       {
-        if (!ActivateKeyboardLayout(japaneseIME, 0))
-        {
-          std::cout << "Failed to activate Japanese IME" << std::endl;
-        }
+        std::cout << "Failed to activate Japanese IME" << std::endl;
+        result->Error("IME Activation Failure", "Failed to activate Japanese IME");
+        return;
       }
 
       HWND hwnd = GetForegroundWindow();
       HIMC himc = ImmGetContext(hwnd);
-      if (method_call.method_name() == "japaneseImeOn")
+
+      DWORD conversionMode = 0, sentenceMode = 0;
+      ImmGetConversionStatus(himc, &conversionMode, &sentenceMode);
+
+      if (method_call.method_name() == "japaneseHiraganaIme")
       {
-        ImmSetOpenStatus(himc, true);
+        conversionMode = IME_CMODE_NATIVE | IME_CMODE_FULLSHAPE;
       }
-      else
+      else if (method_call.method_name() == "japaneseFullWidthKatakana")
       {
-        ImmSetOpenStatus(himc, false);
+        conversionMode = IME_CMODE_NATIVE | IME_CMODE_KATAKANA | IME_CMODE_FULLSHAPE;
       }
+      else if (method_call.method_name() == "japaneseHalfWidthKatakana")
+      {
+        conversionMode = IME_CMODE_NATIVE | IME_CMODE_KATAKANA;
+      }
+      else if (method_call.method_name() == "japaneseHalfWidthAlphanumeric")
+      {
+        conversionMode = IME_CMODE_ALPHANUMERIC;
+      }
+
+      sentenceMode = IME_SMODE_NONE;
+
+      ImmSetConversionStatus(himc, conversionMode, sentenceMode);
+      ImmSetOpenStatus(himc, true);
       ImmReleaseContext(hwnd, himc);
       result->Success();
     }
